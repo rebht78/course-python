acf40a31a2351362ba89971d8ccefc88adfd9a77
Sohel Shaikh <rebht78@gmail.com>
2019-12-02 22:02:52 +0530

    added exercise for numeric

--- exercises/exc_01_08.py
+++ exercises/exc_01_08.py
@@ -1,9 +1,8 @@
-# create favorite_detective variable and assign Sherlock
-___________ = _________
+# create power variable and assign 2
+___________ = _
 
-# create favorite_quote variable and assign Game is ON! with double quotes
-# this we have done for you, using escape characters.
-favorite_quote = '\"Game is ON!\"'
+# create base variable and assign 3
+___________ = _
 
-# print the output, Sherlock: "Game is ON!"
+# print the output, 9 (3 raise to 2)
 print(__________)
